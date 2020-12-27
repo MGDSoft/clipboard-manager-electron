22cf5039ca17b33af94655fcb89cfac76231fb53
Miguel García Domínguez <miguel.garcia@clouddistrict.com>
2020-12-27 11:28:27 +0100

    Config

---
+++ config.js
@@ -0,0 +1,9 @@
+const SHORTCUTS = {
+  CLIPBOARD_COPY_BY_POSITION: 'CmdOrCtrl+Shift+{pos}',
+  CLIPBOARD_SHOW_MANAGER: 'CmdOrCtrl+Shift+Y', // only fow windows
+  CLIPBOARD_SHOW_HISTORY: 'CmdOrCtrl+Shift+L',
+  HISTORY_STRING_LENGTH: 100
+}
+
+
+module.exports = { SHORTCUTS }
