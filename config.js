b0e3c32639a0996b0755112626d8679dc180503c
Miguel García Domínguez <miguel.garcia@clouddistrict.com>
2020-12-27 12:07:17 +0100

    on clipboard history when press number copy that row

--- config.js
+++ config.js
@@ -2,8 +2,10 @@ const SHORTCUTS = {
   CLIPBOARD_COPY_BY_POSITION: 'CmdOrCtrl+Shift+{pos}',
   CLIPBOARD_SHOW_MANAGER: 'CmdOrCtrl+Shift+Y', // only fow windows
   CLIPBOARD_SHOW_HISTORY: 'CmdOrCtrl+Shift+L',
-  HISTORY_STRING_LENGTH: 100
 }
 
+const CONFIG = {
+  HISTORY_STRING_LENGTH: 100,
+}
 
-module.exports = { SHORTCUTS }
+module.exports = { SHORTCUTS, CONFIG }
